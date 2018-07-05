37ea1e88ae7826006ca05714f83793a0c017cdea
= <tqmalcolm@gmail.com>
2018-07-05 13:43:18 -0500

    finished elm_application : now showing games list in an elmy fashion

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,7 +1,34 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+-- MAIN
 
 main : Html msg
 main =
-  text "Hello from Elm inside Phoenix!"
+  div []
+      [ h1 [] [ text "Games"]
+      , gamesIndex model
+      ]
+
+-- MODEL
+model : List String
+model =
+  [ "Platform Game"
+  , "Adventure Game"
+  ]
+
+-- VIEW
+
+gamesIndex : List String -> Html msg
+gamesIndex gameTitles =
+  div [ class "games-index" ] [ gamesList gameTitles ]
+
+gamesList : List String -> Html msg
+gamesList gameTitles =
+  ul [ class "games-list" ] (List.map gamesListItem gameTitles)
+
+gamesListItem : String -> Html msg
+gamesListItem gameTitle =
+  li [] [ text gameTitle ]
